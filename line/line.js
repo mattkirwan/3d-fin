5e1b7c34782e86aba497b5e07f4cfef787651420
Matt Kirwan <lkjsad@fastmail.im>
2024-08-09 12:21:58 +0100

    Added line demo

--- line/line.js
+++ line/line.js
@@ -1,25 +1,28 @@
 
 import * as THREE from 'three';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
+
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
+const camera = new THREE.PerspectiveCamera(45, innerWidth / innerHeight, 1, 500);
+camera.position.set(0, 0, 100);
+camera.lookAt(0, 0, 0);
 
-scene.add(cube);
+const scene = new THREE.Scene();
 
-camera.position.z = 5;
+const material = new THREE.LineBasicMaterial({color: 0x0000ff});
 
-function animate() {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    renderer.render(scene, camera);
-}
+const points = [];
+points.push(new THREE.Vector3(-10, 0, 0));
+points.push(new THREE.Vector3(0, 10, 0));
+points.push(new THREE.Vector3(10, 0, 0));
 
-renderer.setAnimationLoop(animate);
+const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+const line = new THREE.Line(geometry, material);
+
+scene.add(line);
+renderer.render(scene, camera);
